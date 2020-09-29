731adef6ec2d7addb60fc8507a47a1f399137097
Haz-git <zhouharry1@gmail.com>
2020-09-29 10:57:35 -0700

    Added Messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+//Messages:
+
+const messages = [
+  {
+    text: 'Hello!',
+    user: 'Harry',
+    added: new Date()
+  },
+  {
+    text: 'Hi! How are you?',
+    user: 'Dennis',
+    added: new Date()
+  },
+  {
+    text: 'Sup',
+    user: 'Paige',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
